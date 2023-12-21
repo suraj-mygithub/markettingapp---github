c835a4a2cde8346a3feb79c2101c26550fb1579a
SURAJ <Lead-GitHub>
2023-12-21 17:59:57 +0530

    suraj: 1st commit

---
+++ marketingApp-10/src/main/java/com/marketingapp/MarketingApp10Application.java
@@ -0,0 +1,13 @@
+package com.marketingapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarketingApp10Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarketingApp10Application.class, args);
+	}
+
+}
